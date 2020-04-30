2b635a77472d7b07aff07133f925ff54755f6603
Yannick Heiber <yannick.heiber@mobimeo.com>
2020-04-30 15:50:56 +0200

    clipboardPatchFile

--- src/main/scala/calendar.scala
+++ src/main/scala/calendar.scala
@@ -5,32 +5,60 @@ object calendar {
 
     def until(that: HourOfDay): Stream[HourOfDay] = (value until that.value).toStream.map(HourOfDay(_))
   }
+
+
   object HourOfDay {
-    val min = HourOfDay(0)
-    val max = HourOfDay(24)
+    //    val min = HourOfDay(0)
+    //    val max = HourOfDay(24)
+
+    def min(a: HourOfDay, b: HourOfDay): HourOfDay = HourOfDay(Math.min(a.value, b.value))
+
+    def max(a: HourOfDay, b: HourOfDay): HourOfDay = HourOfDay(Math.max(a.value, b.value))
   }
 
   sealed trait DayOfWeek
+
   object DayOfWeek {
-    case object Sunday    extends DayOfWeek
-    case object Monday    extends DayOfWeek
-    case object Tuesday   extends DayOfWeek
+
+    case object Sunday extends DayOfWeek
+
+    case object Monday extends DayOfWeek
+
+    case object Tuesday extends DayOfWeek
+
     case object Wednesday extends DayOfWeek
-    case object Thursday  extends DayOfWeek
-    case object Friday    extends DayOfWeek
-    case object Saturday  extends DayOfWeek
+
+    case object Thursday extends DayOfWeek
+
+    case object Friday extends DayOfWeek
+
+    case object Saturday extends DayOfWeek
+
   }
 
-  final case class TimeSpan(start: HourOfDay, end: HourOfDay)
+  final case class TimeSpan(start: HourOfDay, end: HourOfDay) {
+    def intersect(other: TimeSpan): Option[TimeSpan] =
+      Some(TimeSpan(HourOfDay.max(start, other.start), HourOfDay.min(end, other.end)))
+        .filter(ts => ts.start.value < ts.end.value)
+  }
 
   sealed trait CalendarStatus
+
   object CalendarStatus {
+
     case object Busy extends CalendarStatus
+
     case object Free extends CalendarStatus
+
   }
 
   final case class CalendarRegion(span: TimeSpan, status: CalendarStatus) {
-    def intersect(other: CalendarRegion): CalendarRegion = ???
+    def intersect(other: CalendarRegion): Option[CalendarRegion] = {
+      span.intersect(other.span).filter(_ => status == other.status).map(span =>
+        CalendarRegion(span, status)
+
+      )
+    }
   }
 
   final case class DailySchedule(set: Set[CalendarRegion])
--- src/test/scala/CalendarSpec.scala
+++ src/test/scala/CalendarSpec.scala
@@ -1,10 +1,16 @@
-import org.scalatest.{FlatSpec, Matchers}
 import calendar._
+import org.scalatest.{FlatSpec, Matchers}
 
 class CalendarSpec extends FlatSpec with Matchers {
   "CalenderRegion" should "have intersection if overlapping with same status" in {
     val a = CalendarRegion(TimeSpan(HourOfDay(1), HourOfDay(3)), CalendarStatus.Busy)
     val b = CalendarRegion(TimeSpan(HourOfDay(2), HourOfDay(4)), CalendarStatus.Busy)
-    a.intersect(b) shouldBe CalendarRegion(TimeSpan(HourOfDay(2), HourOfDay(3)), CalendarStatus.Busy)
+    a.intersect(b) should contain (CalendarRegion(TimeSpan(HourOfDay(2), HourOfDay(3)), CalendarStatus.Busy))
+  }
+
+  "CalenderRegion" should "have intersection if overlapping with different status" in {
+    val a = CalendarRegion(TimeSpan(HourOfDay(1), HourOfDay(3)), CalendarStatus.Busy)
+    val b = CalendarRegion(TimeSpan(HourOfDay(2), HourOfDay(4)), CalendarStatus.Free)
+    a.intersect(b) shouldBe None
   }
 }
