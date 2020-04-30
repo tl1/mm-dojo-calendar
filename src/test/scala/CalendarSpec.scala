21eb57a789d9c27ea5940f542627032f3279e743
Tilman Linden <2614667+tl1@users.noreply.github.com>
2020-04-30 16:16:56 +0200

    Breaking naive conflict algorithm.

--- src/test/scala/CalendarSpec.scala
+++ src/test/scala/CalendarSpec.scala
@@ -1,6 +1,8 @@
 import calendar._
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.immutable.{ListSet, TreeSet}
+
 class CalendarSpec extends FlatSpec with Matchers {
   "CalenderRegion" should "have intersection if overlapping with same status" in {
     val a = CalendarRegion(TimeSpan(HourOfDay(1), HourOfDay(3)), CalendarStatus.Busy)
@@ -24,5 +26,12 @@ class CalendarSpec extends FlatSpec with Matchers {
     val a = CalendarRegion(TimeSpan(HourOfDay(1), HourOfDay(3)), CalendarStatus.Busy)
     val c = CalendarRegion(TimeSpan(HourOfDay(5), HourOfDay(6)), CalendarStatus.Busy)
     DailySchedule(Set(a, c)).isConflictFree shouldBe true
+  }
+
+  it should "be able to detect conflict of several schedules" in {
+    val a = CalendarRegion(TimeSpan(HourOfDay(3), HourOfDay(9)), CalendarStatus.Busy)
+    val b = CalendarRegion(TimeSpan(HourOfDay(12), HourOfDay(13)), CalendarStatus.Busy)
+    val c = CalendarRegion(TimeSpan(HourOfDay(2), HourOfDay(4)), CalendarStatus.Busy)
+    DailySchedule(ListSet(a, b, c)).isConflictFree shouldBe false
   }
 }
